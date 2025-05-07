9fd5c87ac4938317aa670f4a5da8aeb0bb87e591
Tyler <tylerhoang456123@gmail.com>
2025-05-06 20:30:32 -0700

    workshop setup for login

--- login/server.js
+++ login/server.js
@@ -12,9 +12,7 @@ app.get('/users', (req, res) => {
 
 app.post('/users', async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    const user = { name: req.body.name, password: hashedPassword }
-    users.push(user)
+    // TODO
     res.status(201).send()
   } catch {
     res.status(500).send()
@@ -27,11 +25,7 @@ app.post('/users/login', async (req, res) => {
     return res.status(400).send('Cannot find user')
   }
   try {
-    if(await bcrypt.compare(req.body.password, user.password)) {
-      res.send('Success')
-    } else {
-      res.send('Not Allowed')
-    }
+    // TODO
   } catch {
     res.status(500).send()
   }
